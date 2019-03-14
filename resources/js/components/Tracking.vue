820bfc0ffadb95024b691f1d652e031a2b588eb1
Louie Miranda <lmiranda@gmail.com>
2019-03-14 17:58:39 +0800

    Vue to list all tracking (by reference_code). Todo on the search next.

---
+++ resources/js/components/Tracking.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h2>Tracking</h2>
+
+        <div class="card card-body" v-for="tracking in trackings" v-bind:key="tracking.id">
+            <p>{{ tracking.reference_code }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                trackings: [],
+                tracking: {
+                    reference_ode: ''
+                }
+            }
+        },
+
+        created() {
+            this.fetchTracking();
+        },
+
+        methods: {
+            fetchTracking() {
+                fetch('api/backend/tracking')
+                    .then(res => res.json())
+                    .then(res => {
+                        this.trackings = res.data;
+                    })
+            }
+        },
+
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
