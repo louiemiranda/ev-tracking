f6965890c0cd83fd5206f50a7ebb9b3b0a95597e
Louie Miranda <lmiranda@gmail.com>
2019-03-14 18:45:59 +0800

    Final frontend fixes/updates.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <script>window.Laravel = { csrfToken: '{{ csrf_token() }}"></script>
+        <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script>
 
         <title>EV-Tracking Web App</title>
 
@@ -17,13 +17,18 @@
     </head>
     <body>
 
-    
-        
-    <div id="app">
-        <div class="container">
-            <tracking></tracking>
+    <div class="jumbotron text-center">
+            <h1>EV Delivery Tracking Web App</h1>
+            <p>Work done by <a href="http://jlxm.xyz" target="_blank">JLXM</a> (lmiranda@gmail.com)</p> 
+            <p>
+            </p>
         </div>
-    </div>
+        
+        <div id="app">
+            <div class="container">
+                <tracking></tracking>
+            </div>
+        </div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
