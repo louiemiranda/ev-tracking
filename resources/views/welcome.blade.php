fa5467022937acae95d6ca8eeb69d2857e276b9e
Louie Miranda <lmiranda@gmail.com>
2019-03-14 17:24:30 +0800

    Update initial frontend template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>EV-Tracking Web App</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -64,6 +64,7 @@
         </style>
     </head>
     <body>
+
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -81,19 +82,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    EV-Tracking Web App
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="http://jlxm.xyz">JLXM</a>
+                    <a href="https://github.com/louiemiranda/ev-tracking">Repository</a>
                 </div>
             </div>
         </div>
+
+
     </body>
 </html>
